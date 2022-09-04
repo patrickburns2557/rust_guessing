3420c02f405305f47fd69bfcb918540a43c168d6
Patrick <patrickburns2557@gmail.com>
2022-09-04 18:25:19 -0500

    Take initial input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-fn main() {
-    println!("Hello, world!");
-    println!("test");
+use std::io;
+
+
+fn main()
+{
+    println!("Guess a number between 1 and 100");
+    println!("Input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    println!("You guessed: {guess}");
 }
