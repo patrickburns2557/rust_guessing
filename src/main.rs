b542a3d662e228d8095f7c95cc97feb237f06948
Patrick <patrickburns2557@gmail.com>
2022-09-04 20:15:28 -0500

    Loops

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,38 @@ fn main()
     println!("Guess a number between 1 and 100");
 
     let secret_number = rand::thread_rng().gen_range(1..=100); //creates a thread_rng object from rand, and executes gen_range function on it
-    println!("Secret number is: {}", secret_number);
-
-    println!("Input your guess:");
-
-    let mut guess = String::new(); //mut makes the variable mutable
-
-    io::stdin()
-        .read_line(&mut guess) //calling read_line and passing in "guess" as the var to store the input in
-        .expect("Failed to read line"); //error message to print if an issue occurs
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    //convert guess from string to unsigned 32-bit int, w/ error message if not able to
-    //"shadows" the previous declaration of guess, to allow us to reuse the "guess" name w/o needing a new variable name
-
-    println!("You guessed: {guess}"); //{} is a placeholder where you can tell the program to print variables
-    //can also be written: println!("You guessed: {}", guess);
-
-
-    match guess.cmp(&secret_number)
+    //println!("Secret number is: {}", secret_number);
+    
+    loop
     {
-        Ordering::Less => println!("Too small, try again."),
-        Ordering::Greater => println!("Too large, try again."),
-        Ordering::Equal => println!("You win!"),
+        println!("Input your guess:");
+
+        let mut guess = String::new(); //mut makes the variable mutable
+
+        io::stdin()
+            .read_line(&mut guess) //calling read_line and passing in "guess" as the var to store the input in
+            .expect("Failed to read line"); //error message to print if an issue occurs
+
+        //go to next iteration of loop if a non-numerical value is input, ignoring any errors that .parse throws
+        //otherwise, stores the converted number back into guess
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}"); //{} is a placeholder where you can tell the program to print variables
+        //can also be written: println!("You guessed: {}", guess);
+
+        //match the output of .cmp to it's corresponding result based on the input number
+        match guess.cmp(&secret_number)
+        {
+            Ordering::Less => println!("Too small, try again."),
+            Ordering::Greater => println!("Too large, try again."),
+            Ordering::Equal =>  {
+                println!("You win!");
+                break; //break out of loop if correct answer found
+            }
+        }
     }
+    
 }
